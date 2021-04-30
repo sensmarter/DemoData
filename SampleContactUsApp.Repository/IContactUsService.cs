17167b4023262db0cce0d0c7af64f64ae573096d
sensmarter <58935252+sensmarter@users.noreply.github.com>
2021-04-30 13:49:32 +0530

    Test Comit

---
+++ SampleContactUsApp.Repository/IContactUsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleContactUsApp.Data.Model;
+namespace SampleContactUsApp.Repository
+{
+    public interface IContactUsService
+    {
+        void SaveData(ContactUsModel contactUsForm);
+    }
+}
---
+++ SampleContactUsApp/MapperConfig/AutoMapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SampleContactUsApp.Data.Model;
+using SampleContactUsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleContactUsApp.MapperConfig
+{
+    public class AutoMapperConfig
+    {
+        public MapperConfiguration Configure()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ContactUsFormViewModel, ContactUsModel>();
+            }
+           );
+
+            return config;
+        }
+    }
+}
